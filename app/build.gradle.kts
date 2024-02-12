d49337ed8ccbbe1cdcd829aab5c0317478af5ad6
Arvind_Kumar_Shishodia_1991 <arvind.kumar@easemytrip.com>
2024-02-12 15:42:54 +0530

    maven gralde

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
     id("base")
     id("maven-publish")
 }
+/*publishing{
+    publications {
+        create("maven"){
+
+        }
+    }
+}*/
 buildscript {
     repositories {
         mavenCentral()
@@ -108,12 +115,12 @@ dependencies {
     //implementation(files("libs/retrofit-2.9.0.jar"))
     //implementation(files("libs/emt-release.aar"))
     testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-//    debugImplementation("androidx.compose.ui:ui-tooling")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     /*dependencies {
         api("com.github.ozodrukh:CircularReveal:2.0.1@aar") {
